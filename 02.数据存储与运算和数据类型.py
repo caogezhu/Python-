4ac8bfa5cfa1bda3d1e7fb002dd9816dad2595f6
caogezhu <caogezhu@qq.com>
2026-06-27 22:24:13 +0800

    新增流程控制语句-while循环-案例

--- 02.数据存储与运算和数据类型.py
+++ 02.数据存储与运算和数据类型.py
@@ -1,3 +1,6 @@
+greet = "您好，崩坏：星穹铁道"
+print(greet + "希儿")
+
 # 对字符串求长度
 s = "Hello World"
 print(len(s))
@@ -18,3 +21,11 @@ print(type(s))
 print(type(b1))
 print(type(n))
 print(type(1.5))
+
+import math
+
+a = 1
+b = 9
+c = 20
+delta = b ** 2 - 4 * a * c
+print((-b + math.sqrt(delta)) / (2 * a), (-b - math.sqrt(delta)) / (2 * a))
